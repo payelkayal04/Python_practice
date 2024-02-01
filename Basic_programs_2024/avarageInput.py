3a6c303be582c51ff9257b7a57ab9b135a9ddee5
Kayal <payel.kayal@capgemini.com>
2024-02-01 19:16:44 +0530

    Added avg input

---
+++ Basic_programs_2024/avarageInput.py
@@ -0,0 +1,16 @@
+"""
+a = int(input("Enter first number: "))
+print(a)
+b = int(input("Enter a second number:"))
+print(b)
+"""
+
+lst = input("Enter the numbers: ").split(',')
+print(lst)
+s = 0
+r = len(lst)
+for i in lst:
+    s = s+int(i)
+print("Sum : ", s)
+average = (s/(len(lst)))
+print("The average is : ", average)
